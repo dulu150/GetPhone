6d72cab30b66ec39381e3366ac016f2f6cf4fe57
nickel zhang <dulu150@126.com>
2017-10-31 22:20:10 +0800

    Create 设置属性.cs

---
+++ 设置属性.cs
@@ -0,0 +1,8 @@
+$("#hwep-buy-513686662").attr("class","button-hwep-add-cart");
+$("#hwep-buy-513686662").attr("text","加入购物车");
+$("#hwep-buy-513686662").attr("onclick","ec.product.addPower(this,513686662,'【内部处理机】HUAWEI G9 Plus 移动定制版 3GB+32GB（月光银）',8);return false;");	
+$("#hwep-buy-513686662").removeAttr("onclick");
+$("#hwep-buy-513686662").click(function(){
+	ec.product.addPower(this,513686662,'【内部处理机】HUAWEI G9 Plus 移动定制版 3GB+32GB（月光银）',8);
+	return false;
+});
